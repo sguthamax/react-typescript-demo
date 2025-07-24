328569ff0f979cdab043c2ba4b3b0ec5240a099b
sguthamax <s08g06@gmail.com>
2025-07-24 15:53:55 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import './App.css';
-import DemoAdvancedComponents from './components/DemoAdvancedComponets';
-import DemoTypeScriptContextAndRefHook from './components/DemoTypeScriptContextAndRefHook';
-import DemoTypeScriptProps from './components/DemoTypeScriptProps';
-import DemoTypeScriptStateHooks from './components/DemoTypeScriptStateHooks';
+ 
 
 function App() {
     return <div className="App">
         <h1>React TypeScript Demo</h1>
-        <DemoTypeScriptProps />
-        <DemoTypeScriptStateHooks />
-        <DemoTypeScriptContextAndRefHook />
-        <DemoAdvancedComponents />
     </div>
 }
 
